4a4c73920f9110d28ba063fa5bb3a3a3908e8b1b
seulgae <r1605866@naver.com>
2025-07-30 15:27:15 +0900

    [20250730]주사위 예제 업로드

---
+++ src/sec07/DiceDemo.java
@@ -0,0 +1,29 @@
+package sec07;
+
+import java.util.Scanner;
+
+//import java.util.Scanner;
+
+public class DiceDemo {
+	public static void main(String[] args) {
+		Dice dice = new Dice();
+//		dice.roll();
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("주사위를 몇 번 반복할래요? : ");
+		int diceNum = in.nextInt();
+		
+		for (int i = 0; i < diceNum; i++) {
+			dice.roll(diceNum);
+		}
+	}
+}
+
+
+class Dice {
+	void roll(int diceNum) {
+		int dice = (int)(Math.random() * 6) + 1;
+		System.out.println(dice);
+	}
+}
+
---
+++ src/sec07/SingletonDemo.java
@@ -0,0 +1,18 @@
+package sec07;
+
+public class SingletonDemo {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Singleton {
+	// static 변수
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton(){
+		
+	}
+	
+	
+}
